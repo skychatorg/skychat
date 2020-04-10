dccbacc75f14a128c46e11ddbae89c35716b2b80
Baptiste Pouget <baba@firemail.cc>
2020-04-10 03:29:53 +0200

    adding yt list (untested)

--- app/server/skychat/commands/impl/YoutubePlugin.ts
+++ app/server/skychat/commands/impl/YoutubePlugin.ts
@@ -102,7 +102,7 @@ export class YoutubePlugin extends Plugin {
         yt: {
             minCount: 1,
             maxCount: 1,
-            params: [{name: 'action', pattern: /^(sync|off|on)$/}]
+            params: [{name: 'action', pattern: /^(sync|off|on|list)$/}]
         },
         play: {
             minCount: 1,
@@ -158,7 +158,12 @@ export class YoutubePlugin extends Plugin {
                     connection.send('yt-sync', null);
                 }
                 break;
-        }
+			case 'list':
+				for (let j=0; j<this.queue.length; j++) {
+					this.room.sendMessage(new Message(this.queue[j].video.title + ', added by ' + this.queue[j].user.username, UserController.getNeutralUser()));
+				}
+				break;
+		}
     }
 
     /**
