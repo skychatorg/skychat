7261ac5e7ce02ebd8c30744fc0acf0b07917126f
7PH <b.raymond@protonmail.com>
2020-08-24 00:17:52 +0200

    Export event data schemas

---
+++ app/model/index.ts
@@ -0,0 +1,10 @@
+
+export { SanitizedMessage } from "../server/skychat/Message";
+export { SanitizedPrivateMessage } from "../server/skychat/PrivateMessage";
+export { MessageSeenEventData } from "../server/skychat/commands/impl/core/MessageSeenPlugin";
+export { SanitizedUser, UserData, AuthToken } from "../server/skychat/User";
+export { SanitizedSession } from "../server/skychat/Session";
+export { SyncPlayerStateObject } from "../server/skychat/commands/impl/youtube/CurrentYoutubeVideo";
+export { YoutubeVideoMeta } from "../server/skychat/commands/impl/youtube/YoutubeVideoMeta";
+export { PendingYoutubeVideo, SanitizedPendingYoutubeVideo } from "../server/skychat/commands/impl/youtube/PendingYoutubeVideo";
+export { SanitizedPoll, PollState } from "../server/skychat/commands/impl/poll/Poll";
