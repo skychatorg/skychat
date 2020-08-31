a11c66f309173674877672b99124b325fe810e01
7PH <b.raymond@protonmail.com>
2020-08-31 22:12:08 +0200

    Preliminary work for cinema mode

---
+++ app/client/src/components/MessagesOverlay.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="messages-overlay">
+        <messages :hide-player="true" ref="messages" @select-message="onSelectMessage" class="messages scrollbar" />
+        <typing-list class="typing-list" />
+        <message-form ref="messageForm" class="message-form"/>
+    </div>
+</template>
+
+<script>
+    import Vue from "vue";
+    import Messages from "./Messages.vue";
+    import TypingList from "./TypingList.vue";
+    import MessageForm from "./MessageForm.vue";
+
+    export default Vue.extend({
+        components: {Messages, TypingList, MessageForm},
+        methods: {
+            onSelectMessage: function() {
+
+            }
+        },
+        computed: {
+        }
+    });
+</script>
+
+<style lang="scss" scoped>
+.messages-overlay {
+
+    .messages {
+        height: 200px;
+    }
+
+    .typing-list {
+        height: 20px;
+    }
+}
+</style>
---
+++ app/client/src/components/PlayerBackground.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <iframe ref="player"
+                class="player"
+                :src="src"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+    </div>
+</template>
+
+<script>
+    import Vue from "vue";
+
+    export default Vue.extend({
+        data: function() {
+            return {
+                src: '',
+                previousPlayedId: null
+            }
+        },
+        watch: {
+            playerState: function() {
+                if (! this.playerState || ! this.playerState.enabled) {
+                    this.src = '';
+                    this.previousPlayedId = null;
+                    return;
+                }
+                if (this.playerState.video.id === this.previousPlayedId) {
+                    return;
+                }
+                let src = 'https://www.youtube.com/embed/' + this.playerState.video.id;
+                src += '?autoplay=1';
+                src += '&origin=' + document.location.origin;
+                if (this.playerState.video.duration > 0) {
+                    src += '&start=' + parseInt(this.playerState.cursor);
+                }
+                this.src = src;
+                this.previousPlayedId = this.playerState.video.id;
+            }
+        },
+        computed: {
+            playerState: function() {
+                return this.$store.state.playerState;
+            }
+        }
+    });
+</script>
+
+<style lang="scss" scoped>
+
+    .player {
+        width: 100%;
+        height: 100%;
+        padding: 5px 5px 0;
+    }
+</style>
