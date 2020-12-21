d7211342ba780e9b2341f5d5a5b55e7643c588fd
7PH <b.raymond@protonmail.com>
2020-12-21 21:28:58 +0100

    Change default cursor to material blue

--- app/client/src/components/CursorLayer.vue
+++ app/client/src/components/CursorLayer.vue
@@ -4,8 +4,8 @@
              v-for="entry in cursors"
              :key="entry.cursor.user.id"
              :style="{left: (entry.cursor.x * window.innerWidth) + 'px', top: (entry.cursor.y * window.innerHeight) + 'px'}">
-            <img src="/assets/images/cursors/default.gif">
-            <span>{{entry.cursor.user.username}}</span>
+            <img src="/assets/images/cursors/default.png">
+            <span class="ml-1">{{entry.cursor.user.username}}</span>
         </div>
     </div>
 </template>
