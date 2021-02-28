90071343c9a234040b0f0100bf12b79e27284fa1
7PH <b.raymond@protonmail.com>
2021-02-28 23:58:48 +0100

    Move avatar images to avatars directory

--- app/server/skychat/commands/impl/customization/AvatarPlugin.ts
+++ app/server/skychat/commands/impl/customization/AvatarPlugin.ts
@@ -4,6 +4,7 @@ import {ConnectedListPlugin} from "../core/ConnectedListPlugin";
 import {Config} from "../../../Config";
 import {MessageFormatter} from "../../../MessageFormatter";
 import {UserController} from "../../../UserController";
+import * as fs from 'fs';
 
 
 export class AvatarPlugin extends Plugin {
@@ -26,6 +27,8 @@ export class AvatarPlugin extends Plugin {
             params: [
                 {
                     name: 'avatar',
+                    // The following regexp ensures that the given path is path to an uploaded avatar on this server
+                    // Security for the image being moved to the avatars directory is ensured through this regexp
                     pattern: new RegExp('^' + MessageFormatter.getInstance().escapeRegExp(Config.LOCATION) + '\/uploads\/([0-9a-zA-Z/-]+)\.(jpg|jpeg|png|webp)$'),
                     info: 'Image link'
                 }
@@ -35,11 +38,24 @@ export class AvatarPlugin extends Plugin {
 
     async run(alias: string, param: string, connection: Connection): Promise<void> {
 
-        const data = UserController.getPluginData(connection.session.user, this.name);
-        if (data === param) {
-            throw new Error('You already have this avatar');
+        // Get local path to image
+        const localPath = '.' + param.substr(Config.LOCATION.length);
+        const avatarExtension = (localPath.match(/\.[a-z]+$/) || [])[0].substr(1);
+        const newAvatarPath = 'avatars/' + connection.session.identifier + '.' + avatarExtension;
+
+        // Check that the given image exists
+        if (! fs.existsSync(localPath)) {
+            throw new Error('Given image does not exist');
         }
-        await UserController.savePluginData(connection.session.user, this.name, param);
-        await (this.room.getPlugin('connectedlist') as ConnectedListPlugin).sync();
+
+        // Copy avatar
+        fs.copyFileSync(localPath, newAvatarPath);
+
+        // Get new avatar url. Append random string for immediately invalidating cache
+        const avatarNewUrl = Config.LOCATION + '/' + newAvatarPath + '?' + new Date().getTime();
+
+        // Save data to database
+        await UserController.savePluginData(connection.session.user, this.name, avatarNewUrl);
+        (this.room.getPlugin('connectedlist') as ConnectedListPlugin).sync();
     }
 }
