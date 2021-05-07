4119182308efbe89ad54ef7b1a50d6c2ba7dd3b3
7PH <b.raymond@protonmail.com>
2021-05-07 23:52:27 +0200

    Minor UI improvements

--- app/client/src/vue/components/channel/PlayerChannelList.vue
+++ app/client/src/vue/components/channel/PlayerChannelList.vue
@@ -10,17 +10,28 @@
             @click.native="joinChannel(channel.id)"
         >
             <div class="channel-content">
-                <div class="channel-icon material-icons md-18">tv</div>
-                <div class="channel-name" :title="channel.name">
-                    <b>{{channel.name}}</b>
+                <div class="channel-content-info">
+                    <div class="channel-icon material-icons md-18">tv</div>
+                    <div class="channel-name" :title="channel.name">
+                        <b>{{channel.name}}</b>
+                    </div>
+                    <div class="channel-meta">
+                        <!-- show if there is a video currently playing -->
+                        <div v-show="channel.playing"
+                            class="channel-player mr-1"
+                            :class="{ 'disabled': playerChannel !== channel.id }"
+                            title="A video is currently playing">
+                            <i class="material-icons md-14">movie</i>
+                        </div>
+                    </div>
                 </div>
-                <div class="channel-meta">
-                    <!-- show if there is a video currently playing -->
-                    <div v-show="channel.playing"
-                        class="channel-player mr-1"
-                        :class="{ 'disabled': playerChannel !== channel.id }"
-                        title="Player state">
-                        <i class="material-icons md-14">movie</i>
+                <div class="channel-content-users" v-if="playerChannelUsers[channel.id] && playerChannelUsers[channel.id].length > 0">
+                    <div v-for="user of playerChannelUsers[channel.id]"
+                        :key="user.username"
+                        class="avatar image-bubble"
+                        :title="user.username + ' is in there'"
+                        :style="{'border': '1px solid ' + user.data.plugins.color}">
+                        <img :src="user.data.plugins.avatar">
                     </div>
                     <!-- connected users -->
                     <div v-if="playerChannelUsers[channel.id] && playerChannelUsers[channel.id].length > 0"
@@ -91,45 +102,70 @@
     }
 
     .channel {
-        height: 35px;
+        min-height: 35px;
         margin-top: 4px;
 
         .channel-content {
             display: flex;
+            flex-direction: column;
 
-            .channel-icon {
-                flex-basis: 20px;
-                margin-top: 10px;
-                margin-left: 10px;
-                font-size: 14px;
-            }
-
-            .channel-name {
-                flex-grow: 1;
-                margin-top: 10px;
-                margin-left: 2px;
-                overflow: hidden;
-                white-space: nowrap;
-                text-overflow: ellipsis;
-            }
-
-            .channel-meta {
-                flex-basis: 65px;
-                margin-top: 10px;
+            .channel-content-info {
                 display: flex;
-                flex-direction: row-reverse;
+                margin-bottom: 4px;
 
-                .channel-player {
-                    color: #ff8f8f;
+
+                .channel-icon {
+                    flex-basis: 20px;
+                    margin-top: 10px;
+                    margin-left: 10px;
+                    font-size: 14px;
                 }
 
-                .channel-player.disabled {
-                    color: #8c8c8c;
+                .channel-name {
+                    flex-grow: 1;
+                    margin-top: 10px;
+                    margin-left: 2px;
+                    overflow: hidden;
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
                 }
+
+                .channel-meta {
+                    flex-basis: 65px;
+                    margin-top: 10px;
+                    display: flex;
+                    flex-direction: row-reverse;
+
+                    .channel-player {
+                        color: #ff8f8f;
+                    }
+
+                    .channel-player.disabled {
+                        color: #8c8c8c;
+                    }
+                }
+            }
+            .channel-content-users {
+                display: flex;
+                overflow: hidden;
+                justify-content: flex-end;
+                margin-bottom: 5px;
+                margin-left: 12px;
 
                 .channel-users {
                     color: #8ecfff;
+                    margin-top: 3px;
+                    margin-left: 6px;
+                    white-space: nowrap;
+                    text-align: right;
                     span { vertical-align: top; }
+                }
+
+                >.avatar {
+                    min-width: 20px;
+                    width: 20px;
+                    height: 20px;
+                    margin: 1px;
                 }
             }
         }
