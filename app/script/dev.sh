8e9322c6a6e3390d560c116841786718be58740b
7PH <b.raymond@protonmail.com>
2024-04-01 18:57:26 +0200

    Implement admin file browser

--- app/script/dev.sh
+++ app/script/dev.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-# Initial build (so nodemon can start)
-babel -x .ts app/api -d build/api
-babel -x .ts app/server -d build/server
+docker compose -f docker-compose.yml -f docker-compose.dev.yml up --build
 
-# Start babel watch, nodemon & vite
-concurrently \
-    "babel --watch --skip-initial-build -x .ts app/api -d build/api" \
-    "babel --watch --skip-initial-build -x .ts app/server -d build/server" \
-    "nodemon --watch build/server build/server/server.js" \
-    "vite"
