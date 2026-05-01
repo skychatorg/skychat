5bc4151d6b0f57e0610ae063e3db78c787e2fcca
7PH <b.raymond@protonmail.com>
2026-05-01 04:54:17 +0200

    Add right-click / long-press context menus on messages, users, rooms

---
+++ app/client/src/components/common/SkyContextMenu.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ContextMenuContent, ContextMenuPortal, ContextMenuRoot, ContextMenuTrigger } from 'radix-vue';
+import { useAttrs } from 'vue';
+
+const attrs = useAttrs();
+
+defineProps<{
+    disabled?: boolean;
+}>();
+</script>
+
+<template>
+    <ContextMenuRoot v-bind="attrs">
+        <ContextMenuTrigger as-child :disabled="disabled">
+            <slot name="trigger" />
+        </ContextMenuTrigger>
+
+        <ContextMenuPortal>
+            <ContextMenuContent class="min-w-[220px] bg-skygray-darker/50 rounded-xl py-1" :collision-padding="8">
+                <slot />
+            </ContextMenuContent>
+        </ContextMenuPortal>
+    </ContextMenuRoot>
+</template>
--- app/client/src/components/room/SingleRoom.vue
+++ app/client/src/components/room/SingleRoom.vue
@@ -2,6 +2,8 @@
 import { useClientState } from '@/composables/useClientState.js';
 import { apiClient, useClientStore } from '@/stores/client';
 import { computed, ref } from 'vue';
+import SkyContextMenu from '../common/SkyContextMenu.vue';
+import SkyContextMenuItem from '../common/SkyContextMenuItem.vue';
 import SkyDropdown from '../common/SkyDropdown.vue';
 import SkyDropdownItem from '../common/SkyDropdownItem.vue';
 import SkyTooltip from '../common/SkyTooltip.vue';
@@ -113,90 +115,118 @@ const leaveRoom = () => {
     </SkyTooltip>
 
     <!-- Full (default) -->
-    <div
-        v-else
-        class="group relative w-full flex items-center gap-2 px-2 py-2 rounded-lg text-left transition cursor-pointer select-none"
-        :class="[
-            selected
-                ? 'bg-primary/10 text-white ring-1 ring-primary/30'
-                : hasUnread
-                ? 'text-white hover:bg-white/5'
-                : 'text-white/60 hover:text-white hover:bg-white/5',
-            isMuted ? 'opacity-50' : '',
-        ]"
-        @click="joinRoom"
-    >
-        <div v-if="selected" class="absolute left-0 top-2 bottom-2 w-[2px] rounded-r bg-primary" />
+    <SkyContextMenu v-else>
+        <template #trigger>
+            <div
+                class="group relative w-full flex items-center gap-2 px-2 py-2 rounded-lg text-left transition cursor-pointer select-none"
+                :class="[
+                    selected
+                        ? 'bg-primary/10 text-white ring-1 ring-primary/30'
+                        : hasUnread
+                        ? 'text-white hover:bg-white/5'
+                        : 'text-white/60 hover:text-white hover:bg-white/5',
+                    isMuted ? 'opacity-50' : '',
+                ]"
+                @click="joinRoom"
+            >
+                <div v-if="selected" class="absolute left-0 top-2 bottom-2 w-[2px] rounded-r bg-primary" />
 
-        <!-- Icon slot -->
-        <span
-            class="w-4 text-center text-xs shrink-0"
-            :class="selected ? 'text-primary' : 'text-white/40'"
-            :title="isProtected ? `Protected room (min right ${room.plugins.roomprotect})` : undefined"
-        >
-            <fa v-if="room.isPrivate && isGroup" icon="users" />
-            <fa v-else-if="room.isPrivate" icon="at" />
-            <fa v-else-if="isProtected" icon="lock" />
-            <span v-else class="font-mono">#</span>
-        </span>
+                <!-- Icon slot -->
+                <span
+                    class="w-4 text-center text-xs shrink-0"
+                    :class="selected ? 'text-primary' : 'text-white/40'"
+                    :title="isProtected ? `Protected room (min right ${room.plugins.roomprotect})` : undefined"
+                >
+                    <fa v-if="room.isPrivate && isGroup" icon="users" />
+                    <fa v-else-if="room.isPrivate" icon="at" />
+                    <fa v-else-if="isProtected" icon="lock" />
+                    <span v-else class="font-mono">#</span>
+                </span>
 
-        <!-- Name -->
-        <span class="flex-1 truncate text-sm">{{ formattedName }}</span>
+                <!-- Name -->
+                <span class="flex-1 truncate text-sm">{{ formattedName }}</span>
 
-        <!-- Muted icon -->
-        <fa v-if="isMuted" icon="volume-xmark" class="text-xs text-white/30" title="Muted" />
+                <!-- Muted icon -->
+                <fa v-if="isMuted" icon="volume-xmark" class="text-xs text-white/30" title="Muted" />
 
-        <!-- User count (shown when no unread) -->
-        <span
-            v-if="userCount > 0 && !hasUnread"
-            class="font-mono text-xs text-white/30 tabular-nums"
-            :title="userCount + ' users in this room'"
-        >
-            {{ userCount }}
-        </span>
+                <!-- User count (shown when no unread) -->
+                <span
+                    v-if="userCount > 0 && !hasUnread"
+                    class="font-mono text-xs text-white/30 tabular-nums"
+                    :title="userCount + ' users in this room'"
+                >
+                    {{ userCount }}
+                </span>
 
-        <!-- Unread dot -->
-        <span v-if="hasUnread" class="w-2 h-2 rounded-full bg-primary shrink-0" title="Unread messages" />
+                <!-- Unread dot -->
+                <span v-if="hasUnread" class="w-2 h-2 rounded-full bg-primary shrink-0" title="Unread messages" />
 
-        <!-- Actions dropdown -->
-        <span @click.stop>
-            <SkyDropdown v-model:open="dropdownOpen">
-                <template #trigger>
-                    <button
-                        class="ml-1 px-1 py-0.5 rounded text-xs transition opacity-0 group-hover:opacity-100"
-                        :class="dropdownOpen ? 'bg-primary/20 text-primary opacity-100' : 'text-white/40 hover:text-white/80'"
-                    >
-                        <fa icon="ellipsis" />
-                    </button>
-                </template>
+                <!-- Actions dropdown -->
+                <span @click.stop>
+                    <SkyDropdown v-model:open="dropdownOpen">
+                        <template #trigger>
+                            <button
+                                class="ml-1 px-1 py-0.5 rounded text-xs transition opacity-0 group-hover:opacity-100"
+                                :class="dropdownOpen ? 'bg-primary/20 text-primary opacity-100' : 'text-white/40 hover:text-white/80'"
+                            >
+                                <fa icon="ellipsis" />
+                            </button>
+                        </template>
 
-                <template #default>
-                    <SkyDropdownItem v-if="!selected" @click="joinRoom">
-                        <fa icon="arrow-right-from-bracket" class="w-4 mr-2" />
-                        Join room
-                    </SkyDropdownItem>
-                    <SkyDropdownItem v-if="selected" @click="leaveRoom">
-                        <fa icon="arrow-left" class="w-4 mr-2" />
-                        Leave room
-                    </SkyDropdownItem>
-                    <SkyDropdownItem v-if="hasUnread" @click="markAsRead">
-                        <fa icon="circle-dot" class="w-4 mr-2" />
-                        Mark as read
-                    </SkyDropdownItem>
-                    <SkyDropdownItem v-if="!isMuted" @click="client.sendMessage(`/mute ${room.id}`)">
-                        <fa icon="volume-xmark" class="w-4 mr-2" />
-                        Mute room
-                    </SkyDropdownItem>
-                    <SkyDropdownItem v-else @click="client.sendMessage(`/unmute ${room.id}`)">
-                        <fa icon="bell" class="w-4 mr-2" />
-                        Unmute room
-                    </SkyDropdownItem>
-                    <SkyDropdownItem @click="copyRoomId">
-                        <fa icon="copy" class="w-4 mr-2" />
-                        Copy room ID
-                    </SkyDropdownItem>
-                </template>
-            </SkyDropdown>
-        </span>
-    </div>
+                        <template #default>
+                            <SkyDropdownItem v-if="!selected" @click="joinRoom">
+                                <fa icon="arrow-right-from-bracket" class="w-4 mr-2" />
+                                Join room
+                            </SkyDropdownItem>
+                            <SkyDropdownItem v-if="selected" @click="leaveRoom">
+                                <fa icon="arrow-left" class="w-4 mr-2" />
+                                Leave room
+                            </SkyDropdownItem>
+                            <SkyDropdownItem v-if="hasUnread" @click="markAsRead">
+                                <fa icon="circle-dot" class="w-4 mr-2" />
+                                Mark as read
+                            </SkyDropdownItem>
+                            <SkyDropdownItem v-if="!isMuted" @click="client.sendMessage(`/mute ${room.id}`)">
+                                <fa icon="volume-xmark" class="w-4 mr-2" />
+                                Mute room
+                            </SkyDropdownItem>
+                            <SkyDropdownItem v-else @click="client.sendMessage(`/unmute ${room.id}`)">
+                                <fa icon="bell" class="w-4 mr-2" />
+                                Unmute room
+                            </SkyDropdownItem>
+                            <SkyDropdownItem @click="copyRoomId">
+                                <fa icon="copy" class="w-4 mr-2" />
+                                Copy room ID
+                            </SkyDropdownItem>
+                        </template>
+                    </SkyDropdown>
+                </span>
+            </div>
+        </template>
+
+        <SkyContextMenuItem v-if="!selected" @select="joinRoom">
+            <fa icon="arrow-right-from-bracket" class="w-4 mr-2" />
+            Join room
+        </SkyContextMenuItem>
+        <SkyContextMenuItem v-if="selected" @select="leaveRoom">
+            <fa icon="arrow-left" class="w-4 mr-2" />
+            Leave room
+        </SkyContextMenuItem>
+        <SkyContextMenuItem v-if="hasUnread" @select="markAsRead">
+            <fa icon="circle-dot" class="w-4 mr-2" />
+            Mark as read
+        </SkyContextMenuItem>
+        <SkyContextMenuItem v-if="!isMuted" @select="client.sendMessage(`/mute ${room.id}`)">
+            <fa icon="volume-xmark" class="w-4 mr-2" />
+            Mute room
+        </SkyContextMenuItem>
+        <SkyContextMenuItem v-else @select="client.sendMessage(`/unmute ${room.id}`)">
+            <fa icon="bell" class="w-4 mr-2" />
+            Unmute room
+        </SkyContextMenuItem>
+        <SkyContextMenuItem @select="copyRoomId">
+            <fa icon="copy" class="w-4 mr-2" />
+            Copy room ID
+        </SkyContextMenuItem>
+    </SkyContextMenu>
 </template>
